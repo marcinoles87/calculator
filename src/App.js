8edaeff0829993837c1467039a97d987a948bf5c
marcinoles87 <marcinoles87@gmail.com>
2023-04-07 16:03:14 +0200

    components add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Button from './components/Button';
+import Input from './components/Input';
 
 function App() {
   return (
     <div className="App">
+      <Input></Input>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
       
-      Calculator
     </div>
   );
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './button.css'
+
+export default function Button() {
+  return (
+    <div>
+        <button>1</button>
+    </div>
+  )
+}
