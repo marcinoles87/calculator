b29eecc0f1e0893be608f3d7842f493429af0081
marcinoles87 <marcinoles87@gmail.com>
2023-04-08 20:36:37 +0200

    components add

--- src/App.js
+++ src/App.js
@@ -8,22 +8,25 @@ function App() {
   let [value , setValue] = useState()
 
   setValue = () =>{
-    value = 1
+    value = 0
   }
+
+  console.log(value)
 
   return (
     <div className="App">
-      <Input value={value}></Input>
-      <Button value={value} setValue={setValue}></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
+      <Input></Input>
+      <Button value={1} setValue={setValue}></Button>
+      <Button value={2} setValue={setValue}></Button>
+      <Button value={3} setValue={setValue}></Button>
+      <Button value={4} setValue={setValue}></Button>
+      <Button value={5} setValue={setValue}></Button>
+      <Button value={6} setValue={setValue}></Button>
+      <Button value={7} setValue={setValue}></Button>
+      <Button value={8} setValue={setValue}></Button>
+      <Button value={9} setValue={setValue}></Button>
+      <Button value={0} setValue={setValue}></Button>
+     
       
     </div>
   );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 
 import './button.css'
 
-export default function Button() {
+export default function Button(props) {
   
-  
+  let {value, setValue} = props
+
+  const handleClick = (e) => {
+    const val = e.target.value
+    console.log(val)
+  }
 
   
   return (
     <div>
-        <button>1</button>
+        <button onClick={handleClick} value={value}>{value}</button>
     </div>
   )
 }
