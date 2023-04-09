075790c42cbd9168c40cb008d4038b7feedcd5d0
marcinoles87 <marcinoles87@gmail.com>
2023-04-09 08:11:15 +0200

    work with function

--- src/App.js
+++ src/App.js
@@ -13,11 +13,12 @@ function App() {
 
   return (
     <div className="App">
+      <h1>MRC CALCULATOR</h1>
       <Input value={value}></Input>
       <Button value={1} setValue={setValue}></Button>
       <Button value={2} setValue={setValue}></Button>
       <Button value={3} setValue={setValue}></Button>
-      <Button value={'+'} setValue={setValue}></Button>
+      <Button value={'+'} setValue={setValue} setPlus={setPlus}></Button>
       <Button value={4} setValue={setValue}></Button>
       <Button value={5} setValue={setValue}></Button>
       <Button value={6} setValue={setValue}></Button>
